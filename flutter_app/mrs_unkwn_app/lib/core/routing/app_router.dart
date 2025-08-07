faa63e65fe3b80f997f1ba7889d7a6bd950802eb
Dennis Wicht <dennis.wicht@web.de>
2025-08-07 23:37:18 +0200

    feat: add basic routing with GoRouter

---
+++ flutter_app/mrs_unkwn_app/lib/core/routing/app_router.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+import '../storage/secure_storage_service.dart';
+import 'route_constants.dart';
+
+/// Central application router using [GoRouter].
+class AppRouter {
+  AppRouter._();
+
+  /// Global [GoRouter] instance.
+  static final GoRouter router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: RouteConstants.login,
+        builder: (context, state) => const Placeholder(),
+      ),
+      GoRoute(
+        path: RouteConstants.register,
+        builder: (context, state) => const Placeholder(),
+      ),
+      GoRoute(
+        path: RouteConstants.home,
+        builder: (context, state) => const Placeholder(),
+        redirect: _authGuard,
+      ),
+      GoRoute(
+        path: RouteConstants.familySetup,
+        builder: (context, state) => const Placeholder(),
+        redirect: _authGuard,
+      ),
+    ],
+    redirect: _rootRedirect,
+  );
+
+  static Future<String?> _authGuard(
+    BuildContext context,
+    GoRouterState state,
+  ) async {
+    final token = await SecureStorageService().read(
+      SecureStorageService.tokenKey,
+    );
+    return token == null ? RouteConstants.login : null;
+  }
+
+  static Future<String?> _rootRedirect(
+    BuildContext context,
+    GoRouterState state,
+  ) async {
+    final token = await SecureStorageService().read(
+      SecureStorageService.tokenKey,
+    );
+    final location = state.location;
+    if (token == null &&
+        location != RouteConstants.login &&
+        location != RouteConstants.register) {
+      return RouteConstants.login;
+    }
+    if (token != null && location == RouteConstants.login) {
+      return RouteConstants.home;
+    }
+    return null;
+  }
+}
---
+++ flutter_app/mrs_unkwn_app/lib/core/routing/route_constants.dart
@@ -0,0 +1,6 @@
+class RouteConstants {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+  static const String familySetup = '/family-setup';
+}
